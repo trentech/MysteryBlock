33367aed03ac1924d880a47691967b42f3b42e66
TrenTech <TrenTech@gmail.com>
2016-10-25 15:28:05 -0400

    Fix config initialization

--- src/main/java/com/gmail/trentech/mysteryblock/Main.java
+++ src/main/java/com/gmail/trentech/mysteryblock/Main.java
@@ -3,22 +3,14 @@ package com.gmail.trentech.mysteryblock;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.spongepowered.api.Sponge;
-import org.spongepowered.api.block.BlockType;
 import org.spongepowered.api.config.ConfigDir;
-import org.spongepowered.api.effect.potion.PotionEffectType;
-import org.spongepowered.api.entity.EntityType;
-import org.spongepowered.api.entity.EntityTypes;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameInitializationEvent;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
-import org.spongepowered.api.item.ItemType;
-import org.spongepowered.api.item.ItemTypes;
 import org.spongepowered.api.plugin.Dependency;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginContainer;
@@ -68,53 +60,10 @@ public class Main {
 
 	@Listener
 	public void onStartedServer(GameStartedServerEvent event) {
-		ConfigManager configManager = ConfigManager.get("entities");
-
-		List<String> list = new ArrayList<>();
-
-		for (EntityType entityType : Sponge.getRegistry().getAllOf(EntityType.class)) {
-			if ((!entityType.equals(EntityTypes.HUMAN)) && (!entityType.equals(EntityTypes.PLAYER)) && (!entityType.equals(EntityTypes.ITEM))) {
-				list.add(entityType.getId());
-			}
-		}
-		
-		configManager.getConfig().getNode("entities").setValue(list);
-		configManager.save();
-		
-		configManager = ConfigManager.get("items");
-
-		list = new ArrayList<>();
-
-		for (ItemType itemType : Sponge.getRegistry().getAllOf(ItemType.class)) {
-			if (!itemType.equals(ItemTypes.NONE)) {
-				list.add(itemType.getId());
-			}
-		}
-		
-		configManager.getConfig().getNode("items").setValue(list);
-		configManager.save();
-
-		configManager = ConfigManager.get("blocks");
-
-		list = new ArrayList<>();
-
-		for (BlockType blockType : Sponge.getRegistry().getAllOf(BlockType.class)) {
-			list.add(blockType.getId());
-		}
-		
-		configManager.getConfig().getNode("blocks").setValue(list);
-		configManager.save();
-		
-		configManager = ConfigManager.get("potions");
-
-		list = new ArrayList<>();
-
-		for (PotionEffectType effectType : Sponge.getRegistry().getAllOf(PotionEffectType.class)) {
-			list.add(effectType.getId());
-		}
-
-		configManager.getConfig().getNode("potions").setValue(list);
-		configManager.save();
+		ConfigManager.init("entities");
+		ConfigManager.init("items");
+		ConfigManager.init("blocks");
+		ConfigManager.init("potions");
 	}
 
 	public Logger getLog() {
--- src/main/java/com/gmail/trentech/mysteryblock/utils/ConfigManager.java
+++ src/main/java/com/gmail/trentech/mysteryblock/utils/ConfigManager.java
@@ -3,9 +3,18 @@ package com.gmail.trentech.mysteryblock.utils;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.block.BlockType;
 import org.spongepowered.api.block.BlockTypes;
+import org.spongepowered.api.effect.potion.PotionEffectType;
+import org.spongepowered.api.entity.EntityType;
+import org.spongepowered.api.entity.EntityTypes;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.ItemTypes;
 
 import com.gmail.trentech.mysteryblock.Main;
 
@@ -52,31 +61,69 @@ public class ConfigManager {
 		ConfigManager configManager = new ConfigManager(configName);
 		CommentedConfigurationNode config = configManager.getConfig();
 
-		if (config.getNode("blocks").isVirtual()) {
-			if (config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId()).isVirtual()) {
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "percentage").setValue(Double.valueOf(0.2D)).setComment("0.0 to 1.0");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "type").setValue("ITEM").setComment("ENTITY, ITEM or POTION");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "data").setValue("minecraft:apple");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "amplifier").setValue(Integer.valueOf(1));
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "cancel").setValue(Boolean.valueOf(false));
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "percentage").setValue(Double.valueOf(0.2D)).setComment("0.0 to 1.0");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "type").setValue("ITEM").setComment("ENTITY, ITEM or POTION");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "data").setValue("minecraft:golden_apple");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "amplifier").setValue(Integer.valueOf(1));
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "cancel").setValue(Boolean.valueOf(false));
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "percentage").setValue(Double.valueOf(0.2D)).setComment("0.0 to 1.0");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "type").setValue("ITEM").setComment("ENTITY, ITEM or POTION");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "data").setValue("minecraft:blaze_powder");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "amplifier").setValue(Integer.valueOf(1));
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "cancel").setValue(Boolean.valueOf(false));
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "percentage").setValue(Double.valueOf(0.2D)).setComment("0.0 to 1.0");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "type").setValue("ITEM").setComment("ENTITY, ITEM or POTION");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "data").setValue("minecraft:pumpkin");
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "amplifier").setValue(Integer.valueOf(1));
-				config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "cancel").setValue(Boolean.valueOf(false));
+		if(configName.equals("config")) {
+			if (config.getNode("blocks").isVirtual()) {
+				if (config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId()).isVirtual()) {
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "percentage").setValue(Double.valueOf(0.2D)).setComment("0.0 to 1.0");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "type").setValue("ITEM").setComment("ENTITY, ITEM or POTION");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "data").setValue("minecraft:apple");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "amplifier").setValue(Integer.valueOf(1));
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example1", "cancel").setValue(Boolean.valueOf(false));
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "percentage").setValue(Double.valueOf(0.2D)).setComment("0.0 to 1.0");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "type").setValue("ITEM").setComment("ENTITY, ITEM or POTION");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "data").setValue("minecraft:golden_apple");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "amplifier").setValue(Integer.valueOf(1));
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example2", "cancel").setValue(Boolean.valueOf(false));
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "percentage").setValue(Double.valueOf(0.2D)).setComment("0.0 to 1.0");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "type").setValue("ITEM").setComment("ENTITY, ITEM or POTION");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "data").setValue("minecraft:blaze_powder");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "amplifier").setValue(Integer.valueOf(1));
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example3", "cancel").setValue(Boolean.valueOf(false));
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "percentage").setValue(Double.valueOf(0.2D)).setComment("0.0 to 1.0");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "type").setValue("ITEM").setComment("ENTITY, ITEM or POTION");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "data").setValue("minecraft:pumpkin");
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "amplifier").setValue(Integer.valueOf(1));
+					config.getNode("blocks", BlockTypes.DIAMOND_ORE.getId(), "example4", "cancel").setValue(Boolean.valueOf(false));
+				}
 			}
+		} else if(configName.equals("entities")) {
+			List<String> list = new ArrayList<>();
+			
+			for (EntityType entityType : Sponge.getRegistry().getAllOf(EntityType.class)) {
+				if ((!entityType.equals(EntityTypes.HUMAN)) && (!entityType.equals(EntityTypes.PLAYER)) && (!entityType.equals(EntityTypes.ITEM))) {
+					list.add(entityType.getId());
+				}
+			}
+			
+			config.getNode("entities").setValue(list);
+		} else if(configName.equals("items")) {
+			List<String> list = new ArrayList<>();
+
+			for (ItemType itemType : Sponge.getRegistry().getAllOf(ItemType.class)) {
+				if (!itemType.equals(ItemTypes.NONE)) {
+					list.add(itemType.getId());
+				}
+			}
+			
+			configManager.getConfig().getNode("items").setValue(list);
+		} else if(configName.equals("blocks")) {
+			List<String> list = new ArrayList<>();
+
+			for (BlockType blockType : Sponge.getRegistry().getAllOf(BlockType.class)) {
+				list.add(blockType.getId());
+			}
+			
+			configManager.getConfig().getNode("blocks").setValue(list);
+		} else if(configName.equals("potions")) {
+			List<String> list = new ArrayList<>();
+
+			for (PotionEffectType effectType : Sponge.getRegistry().getAllOf(PotionEffectType.class)) {
+				list.add(effectType.getId());
+			}
+
+			configManager.getConfig().getNode("potions").setValue(list);
 		}
-		
+
 		configManager.save();
 		
 		return configManager;
